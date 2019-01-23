eb89c5854fa2b1e5206e70af8648a292d1379ec1
6br <tfdvrt@gmail.com>
2019-01-23 16:50:27 +0900

    Fix Design

--- src/main.js
+++ src/main.js
@@ -473,9 +473,9 @@ const INTERVAL = 150;
 
 export default () => {
     /* Initialize D3.JS */
-    var margin = {top:0, right:50, bottom:-50, left:50},
+    var margin = {top:0, right:50, bottom:-100, left:50},
         width = 960 - margin.left - margin.right,
-        height = 200 - margin.top - margin.bottom;
+        height = 100 - margin.top - margin.bottom;
 
     var svg = d3.select("#vis")
         .append("svg")
